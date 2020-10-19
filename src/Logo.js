a8dbebf8771cdf6b0ada7b0a6b6953a6dcaf082b
Samuel Guo <samuelzguo@gmail.com>
2020-10-19 00:49:30 -0700

    Update Logo.js

--- src/Logo.js
+++ src/Logo.js
@@ -5,7 +5,7 @@ function Logo() {
 	return (
 		<div>
 			<img src="https://i.imgur.com/JJ0Sjjn.png" height="150px"></img>
-			<Typist cursor.hideWhenDone=true>
+			<Typist cursor={{ hideWhenDone: true }}>
 				<Typist.Delay ms={100} />
 				<p id="title"><b>prompt box</b></p>
 				<Typist.Delay ms={300} />
